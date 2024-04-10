e599d4335557be550745dbb9691e57ab2fec7c05
NoahHorowitz <horowitz.noah@ufl.edu>
2024-04-10 14:42:51 -0400

    Finished options 1 and 3, created proper menu

--- Lab9.py
+++ Lab9.py
@@ -1,17 +1,39 @@
-while True:
-    password = input('Input your password: ')
-    encoded_pass = ''
-    try:
-        if len(password) == 8 and int(password) >= 0:
-            for digit in password:
-                if int(digit) >= 7:
-                    encoded_pass += str(int(digit) + 3 - 10)
+if __name__ == "__main__":
+
+    while True:
+        # Menu
+        print('Password Encoder/Decoder')
+        print('------------------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        option = input('Choose an option: ')
+        print('')
+
+
+        # Option 1: encoder
+        if option == '1':
+            password = input('Input your password: ')
+            encoded_password = str()
+            try:
+                if len(password) == 8 and int(password) >= 0:
+                    for digit in password:
+                        if int(digit) >= 7:
+                            encoded_password += str(int(digit) + 3 - 10)
+                        else:
+                            encoded_password += str(int(digit) + 3)
+                    password = str()
+                    print(f'Your encoded password: {encoded_password}')
                 else:
-                    encoded_pass += str(int(digit) + 3)
-            print(f'Your encoded password: {encoded_pass}')
-        elif password == '-1':
+                    print('Invalid password: Please use 8 non-negative numbers')
+            except:
+                print('Invalid password: An Error occurred')
+            print('')
+
+
+        # Option 2: decoder
+
+        # Option 3: exit the program
+        elif option == '3':
+            print('Thank you for using this program')
             break
-        else:
-            print('Invalid password')
-    except:
-        print('Invalid password')
